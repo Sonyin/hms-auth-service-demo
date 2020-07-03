ce47940fcb5590c6a310105e476c209460be18ee
Tuğrul Altun <37022367+tualdev@users.noreply.github.com>
2020-07-03 16:33:40 +0300

    update Constants

--- app/src/main/java/com/huawei/authservicesuperdemoo/Constants.kt
+++ app/src/main/java/com/huawei/authservicesuperdemoo/Constants.kt
@@ -1,6 +1,6 @@
 package com.huawei.authservicesuperdemoo
 
-const val WEIBO_APP_KEY = "2045436852"
+const val WEIBO_APP_KEY = ""
 const val WEIBO_REDIRECT_URL = "http://www.sina.com"
 const val WEIBO_SCOPE = ("email,direct_messages_read,direct_messages_write,"
         + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
@@ -11,13 +11,13 @@ const val SCOPE_DEFAULT = "email"
 const val GOOGLE_REQUEST_SIGNIN = 10000
 const val GOOGLE_PLAY_GAME_REQUEST_SIGNIN = 10001
 
-val SINA_APP_KEY = "794670458"
+val SINA_APP_KEY = ""
 val SINA_REDIRECT_URL = "http://open.weibo.com/apps/794670458/info/advanced"
 val SINA_SCOPE = "all"
 
 // QQ
-val QQ_APP_ID = "1106281197"
+val QQ_APP_ID = ""
 val QQ_SCOPE = "all"
 
-val WECHAT_APP_ID = "wxfbc0cf1262af91c3"
-val WECHAT_APP_SECRET = "23fa76dd0a33a92d9c936d1a372537ac"
+val WECHAT_APP_ID = ""
+val WECHAT_APP_SECRET = ""
